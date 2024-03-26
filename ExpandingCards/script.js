280b6194ae0c43e447ff4b55f29862e25185f445
James Cho <james.je.cho@gmail.com>
2024-03-26 13:39:46 -0700

    :+1:

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,5 +1,34 @@
 const panels = document.querySelectorAll('.panel');
-console.log(panels);
+const titles = document.querySelectorAll('.title');
+const unsplashURL = `https://source.unsplash.com/random`;
+const funnyAdjectives = [
+    "zany",
+    "kooky",
+    "wacky",
+    "quirky",
+    "goofy",
+    "whimsical",
+    "funky",
+    "bizarre",
+    "absurd",
+    "ridiculous",
+    "witty",
+    "silly",
+    "droll",
+    "eccentric",
+    "offbeat",
+    "outrageous",
+    "lopsided",
+    "lopsided",
+    "wonky",
+    "wonky"
+  ];
+
+panels.forEach(panel => {
+    panel.style.backgroundImage = `url(${unsplashURL}/${getRandomSize()})`;
+  });
+
+
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses();
@@ -7,8 +36,22 @@ panels.forEach(panel => {
     })
 })
 
+titles.forEach(title => {
+    const randomWord = funnyAdjectives[Math.floor(Math.random() * funnyAdjectives.length)];
+    console.log(randomWord);
+    title.innerHTML = randomWord;
+})
+
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active');
     })
+}
+
+function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300
 }
--- HeartClick/script.js
+++ HeartClick/script.js
@@ -1,37 +1,46 @@
-const loveMe = document.querySelector('.loveMe');
 const times = document.querySelector('#times');
 
 let clickTime = 0;
 let timesClicked = 0;
 
 let randomURL = 'https://source.unsplash.com/random/';
+const images = document.querySelectorAll(".loveMe");
 
-axios.get(randomURL).then( data => {
-  // the url of the random img
-  console.log(data.request.responseURL);
-  loveMe.style.backgroundImage = "url('" + data.request.responseURL + "')" // the url of the random imga
-});
+images.forEach(image => {
 
-loveMe.addEventListener('click', (e) => {
-    if (clickTime === 0) {
-        clickTime = new Date().getTime();
-    } else {
-        if ((new Date().getTime() - clickTime) < 800) {
-            createHeart(e);
-            clickTime = 0;
-        } else {
+    image.style.backgroundImage = "url(" + randomURL + getRandomSize() + ")"; 
+
+    image.addEventListener('click', (e) => {
+        if (clickTime === 0) {
             clickTime = new Date().getTime();
+        } else {
+            if ((new Date().getTime() - clickTime) < 800) {
+                createHeart(e);
+                updateTotalTimesClicked();
+                clickTime = 0;
+            } else {
+                clickTime = new Date().getTime();
+            }
         }
-    }
-});
+    });
+})
+
+
+function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300
+}
 
 const createHeart = (e) => {
     const heart = document.createElement('i');
     heart.classList.add('fas');
     heart.classList.add('fa-heart');
 
-    const x = e.clientX;
-    const y = e.clientY;
+    const x = e.pageX;
+    const y = e.pageY;
 
     const leftOffset = e.target.offsetLeft;
     const topOffset = e.target.offsetTop;
@@ -42,10 +51,37 @@ const createHeart = (e) => {
     heart.style.top = `${yInside}px`;
     heart.style.left = `${xInside}px`;
 
-    loveMe.appendChild(heart);
+    e.target.appendChild(heart);
 
-    times.innerHTML = ++timesClicked;
-
+    const divElement = e.target;
+    const timesClicked = parseInt(divElement.dataset.timesClicked);
+    if (!isNaN(timesClicked)) {
+        divElement.dataset.timesClicked = timesClicked + 1;
+    } else {
+        divElement.dataset.timesClicked = 1; // Set to a default value if NaN
+    }
+    console.log( divElement.dataset.timesClicked )
     setTimeout(() => heart.remove(), 1000);
 }
+
+
+// Function to calculate and update the total times clicked
+const updateTotalTimesClicked = () => {
+    const loveMeDivs = document.querySelectorAll('.loveMe');
+    let totalTimesClicked = 0;
+
+    loveMeDivs.forEach(divElement => {
+        const timesClicked = parseInt(divElement.dataset.timesClicked);
+        if (!isNaN(timesClicked)) {
+            totalTimesClicked += timesClicked;
+        }
+    });
+
+    // Update the total in the #times element
+    const timesElement = document.querySelector('#times');
+    timesElement.textContent = totalTimesClicked;
+};
+
+
+updateTotalTimesClicked();
 
