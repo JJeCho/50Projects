9cecc9fa87f1708d39e6a0c7e513b7d66a25f3cd
JJeCho <james.je.cho@gmail.com>
2024-05-20 09:51:51 -0700

    Update logic to choice picker

--- ChoicePicker/script.js
+++ ChoicePicker/script.js
@@ -1,18 +1,20 @@
 const tagsEl = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
+const button = document.getElementById('button');
 
 textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
-
     if (e.key === 'Enter') {
         setTimeout(() => {
             e.target.value = '';
         }, 10);
-
-        randomSelect();
     }
+});
+
+button.addEventListener('click', () => {
+    randomSelect();
 });
 
 function createTags(input) {
